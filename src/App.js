d5c71108362aff3515b02d181805e443ccdc2211
yanishevska <yanishevska1989@gmail.com>
2021-10-14 13:40:42 +0200

    add base file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+import Container from "./components/Container";
+
+class App extends Component {
+  render() {
+     return (
+      <Container>
+        <h1>Image Finder</h1>
+      </Container>
+    );
+  }
+}
+
+export default App;
